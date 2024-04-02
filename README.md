1a8b8dd1dea3ef858052101c09089e2d93c992a0
rvaldez20 <rvaldez20@gmail.com>
2024-04-01 21:24:36 -0600

    [INIT] Init proyect, clean files and config talwindcss

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + TypeScript + Vite
+
+## Calorie Tracker
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-blue-500">Calorie Tracker</h1>
+    </>
+  )
+}
+
+export default App
