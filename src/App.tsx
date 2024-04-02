817a4b029570288e1b91189a6381c7725f1e8a07
rvaldez20 <rvaldez20@gmail.com>
2024-04-01 21:35:32 -0600

    [ADD] add header with name app and section that include the form

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,19 @@ function App() {
 
   return (
     <>
-      <h1 className="text-blue-500">Calorie Tracker</h1>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase">
+            Calorie Tracker+
+          </h1>
+        </div>
+      </header>
+
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <p>Formulario aquí</p>
+        </div>
+      </section>
     </>
   )
 }
