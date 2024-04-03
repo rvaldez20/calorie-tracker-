99cb55e409686df801de7dc3ca1f301103618f35
rvaldez20 <rvaldez20@gmail.com>
2024-04-03 15:18:45 -0600

    [ADD] start to create reducer(activity-reducer)

---
+++ src/reducers/activity-reducer.ts
@@ -0,0 +1,24 @@
+import { Activity } from "../types"
+
+
+export type ActivityActions = {
+
+}
+
+type ActivityState = {
+   activities: Activity[]
+}
+
+
+export const initialState:ActivityState = {
+   activities: []
+}
+
+
+
+export const activityReducer = (
+      state: ActivityState = initialState,
+      action: ActivityActions
+   ) => {
+   
+}
