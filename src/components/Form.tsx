e7e872f328d1fe7bdaa772b9a35f20a69f2303d2
rvaldez20 <rvaldez20@gmail.com>
2024-04-03 13:28:20 -0600

    [ADD] validate that caterory and calories will be type of number before to set the state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,23 @@
 import { useState, ChangeEvent } from 'react'
+import { Activity } from '../types/index'
 import { categories } from '../data/categories'
 
 
 
 export default function Form() {
-   const [activity, setActivity] = useState({
+   const [activity, setActivity] = useState<Activity>({
       category: 1,
       name: '',
       calories: 0,
    })
 
    const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+      const isNumberField = ['category', 'calories'].includes(e.target.id)
+      
       setActivity({
          ...activity,
-         [e.target.id]: e.target.value
+         [e.target.id]: isNumberField ? +e.target.value : e.target.value
       })
-      console.log(e.target.id)
-      console.log(e.target.value)
    }
 
 
