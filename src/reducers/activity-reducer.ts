9ff5259e65322035ddcb8fe95953377d0a143889
rvaldez20 <rvaldez20@gmail.com>
2024-04-03 15:26:42 -0600

    [ADD] create the action(save-activity) and conected with activityReducer

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,13 +1,13 @@
 import { Activity } from "../types"
 
 
-export type ActivityActions = {
-
-}
+export type ActivityActions = 
+   {type: 'save-activity', payload: { newActivity: Activity }}
 
 type ActivityState = {
    activities: Activity[]
 }
+
 
 
 export const initialState:ActivityState = {
@@ -20,5 +20,11 @@ export const activityReducer = (
       state: ActivityState = initialState,
       action: ActivityActions
    ) => {
+
+   if(action.type === 'save-activity') {
+      // Este codigo maneja la logica para actualizar el state
+      console.log('Desde el type de save-activity')
+
+   }
    
 }
