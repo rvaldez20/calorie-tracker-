ee7f443ba21ebd5ccbb56f4a6b630f3039020bd4
rvaldez20 <rvaldez20@gmail.com>
2024-04-05 11:56:02 -0600

    [ADD] add the action set-activeId and define el return for this action

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,15 +1,19 @@
 import { Activity } from "../types"
 
 export type ActivityActions = 
-   {type: 'save-activity', payload: { newActivity: Activity }}
+   { type: 'save-activity', payload: {newActivity: Activity} } |
+   { type: 'set-activeId', payload: {id: Activity['id']} } 
+   
 
 type ActivityState = {
    activities: Activity[],
+   activeId: Activity['id']
 }
 
 
 export const initialState:ActivityState = {
    activities: [],
+   activeId: ''
 }
 
 
@@ -25,6 +29,14 @@ export const activityReducer = (
       return {
          ...state,
          activities: [...state.activities, action.payload.newActivity]
+      }
+   }
+
+   if(action.type === 'set-activeId') {
+
+      return {
+         ...state,
+         activeId: action.payload.id
       }
    }
    
