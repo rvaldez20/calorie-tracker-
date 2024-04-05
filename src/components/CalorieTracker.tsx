b66f122f9c279ee05fb2db79d34d0bcdcdf79569
rvaldez20 <rvaldez20@gmail.com>
2024-04-05 15:22:08 -0600

    [ADD] use the component CalorieDisplay for show calories consumed and calories burned

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react"
 import type { Activity } from "../types"
+import CalorieDisplay from "./CalorieDisplay"
 
 type CalorieTrackerProps = {
    activities: Activity[]
@@ -18,14 +19,14 @@ export default function CalorieTracker({activities}:CalorieTrackerProps) {
          <h2 className="text-4xl font-black text-white text-center">Calories Summary</h2>
 
          <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
-            <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-               <span className="font-black text-6xl text-orange-500">{caloriesCosumed}</span>
-               Consumed
-            </p>
-            <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-               <span className="font-black text-6xl text-lime-500">{caloriesBurned}</span>
-               Burned
-            </p>
+            <CalorieDisplay 
+               calories={caloriesCosumed}
+               text="Consumed"
+            />
+            <CalorieDisplay 
+               calories={caloriesBurned}
+               text="Burned"
+            />         
          </div>
       </>
    )
