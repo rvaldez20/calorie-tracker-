cec370aed32fe5f20887e40ee9c4dc5dad0c6ee1
rvaldez20 <rvaldez20@gmail.com>
2024-04-05 12:58:40 -0600

    [ADD] pass via props the state to component <Form />

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
         <div className="max-w-4xl mx-auto">
           <Form 
             dispatch={dispatch}
+            state={state}
           />
         </div>
       </section>
