73c5ee13f4dbebf6baef3bc8acef61c33b68b00a
rvaldez20 <rvaldez20@gmail.com>
2024-04-04 18:11:31 -0600

    [ADD] add id to object activity in initialState and type

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { useState, ChangeEvent, FormEvent, Dispatch } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import { ActivityActions } from '../reducers/activity-reducer'
 import { Activity } from '../types/index'
 import { categories } from '../data/categories'
@@ -7,12 +8,16 @@ type FormProps = {
    dispatch: Dispatch<ActivityActions>
 }
 
+
+const initialState: Activity = {
+   id: uuidv4(),
+   category: 1,
+   name: '',
+   calories: 0,
+}
+
 export default function Form({dispatch}:FormProps) {
-   const [activity, setActivity] = useState<Activity>({
-      category: 1,
-      name: '',
-      calories: 0,
-   })
+   const [activity, setActivity] = useState<Activity>(initialState)
 
    const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
       const isNumberField = ['category', 'calories'].includes(e.target.id)
@@ -34,6 +39,10 @@ export default function Form({dispatch}:FormProps) {
       e.preventDefault()
 
       dispatch({ type: "save-activity", payload: {newActivity: activity }})
+      setActivity({
+         ...initialState,
+         id: uuidv4(),
+      })
    }
 
 
